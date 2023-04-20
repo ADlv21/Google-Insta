30ff6f37017d855e9c6908c51ab6934ae9fa26ad
ADwait <adwaitg02@gmail.com>
2023-04-21 01:44:43 +0530

    Homepage Complete

--- static/app-setup.js
+++ static/app-setup.js
@@ -1,11 +1,11 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyC07hVRQszzkAlUqHOUxA9uRARzkOmm7lk",
-  authDomain: "api-project-273158969604.firebaseapp.com",
-  databaseURL: "https://api-project-273158969604.firebaseio.com",
-  projectId: "api-project-273158969604",
-  storageBucket: "api-project-273158969604.appspot.com",
-  messagingSenderId: "273158969604",
-  appId: "1:273158969604:web:9a555442019afe71909851"
+const firebaseConfig = {
+  apiKey: "AIzaSyAvJhTtJoozd_h0LDFAzhFI8pvDkceM9Pg",
+  authDomain: "insta-cloud-99d25.firebaseapp.com",
+  projectId: "insta-cloud-99d25",
+  storageBucket: "insta-cloud-99d25.appspot.com",
+  messagingSenderId: "799654185244",
+  appId: "1:799654185244:web:cb94e8c7b67fb1a3f112d9",
+  measurementId: "G-LWEGXG18E4",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
